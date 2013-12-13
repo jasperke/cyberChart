75a707a19bb670e745f11f69f3e1effa68d315d4
Jasper Ke <jasperke@kinghood.com.tw>
2013-12-13 14:13:14 +0800

    use grunt-bump

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
 				 '* Project: <%= pkg.name %>',
 				 '* Version: <%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd HH:MM") %>)',
 				 '* Development By: <%= pkg.author %>',
-				 '* Copyright(c): <%= grunt.template.today("yyyy") %>',
+				 '* Copyright(c): <%= grunt.template.today("yyyy") %> Jasper Ke All rights reserved.',
 				 '*/',
 				 ''
 		],
@@ -44,12 +44,28 @@ module.exports = function(grunt) {
 					spawn: true,
 				}
 			}
+		},
+		bump: {
+			options: {
+				files: ['package.json'],
+				updateConfigs: [],
+				commit: true,
+				commitMessage: 'Release v%VERSION%',
+				commitFiles: ['package.json'], // '-a' for all files
+				createTag: true,
+				tagName: 'v%VERSION%',
+				tagMessage: 'Version %VERSION%',
+				push: false,
+				pushTo: 'master',
+				gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-bump');
 
 //	grunt.registerTask('dev', ['copy:dev']);
 	grunt.registerTask('default', ['copy:dist', 'uglify']);
